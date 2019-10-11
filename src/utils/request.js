f426dedbb4ad3352ad8fbd4c8ec1bbd8f63eb8f0
sherleenStar <sherleenStar@163.com>
2019-10-11 17:03:02 +0800

    处理后台返回数据超过js数据范围,安装json-bigint 大数字转换

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import jsonBig from 'json-bigint'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
-
+request.defaults.transformResponse = [function (data) {
+  return data ? jsonBig.parse(data) : {}
+}]
 export default request
